d8d45b0506dc7b0e0f02877747af856d949d5048
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2025-12-06 19:41:45 -0300

    remove redirect_view path from urlpatterns

--- server/core/urls.py
+++ server/core/urls.py
@@ -1,11 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from url_shortener.views import redirect_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('url_shortener.urls')),
-    path('s/<str:short_url>/', redirect_view, name='redirect_view'),
-
 ]
